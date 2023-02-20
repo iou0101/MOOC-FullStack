f0cd5556c37da8f8497ba1ed4a889d5359ca5c63
Ibrahim <ibrahim.aldarra010101@gmail.com>
2023-02-20 12:56:20 +0000

    Update App.jsx

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -3,41 +3,62 @@ import Contact from "./components/Number";
 
 const App = () => {
   // TODO: contacts state needs to be reworked
-  const [contacts, setContacts] = useState([{ id: 1, name: "Arto Hellas" }]);
+  const [contacts, setContacts] = useState([
+    { id: 1, name: "Arto Hellas", telephone: "06987654321" },
+  ]);
 
-  const [newName, setNewName] = useState("");
+  const [newContact, setNewContact] = useState({
+    id: contacts.length + 1,
+    name: "",
+    telephone: "",
+  });
 
+  // return true on duplicate names
   const checkDuplicateNames = (name) => {
     const contactsNames = contacts.map((contact) => contact.name);
     return contactsNames.includes(name);
   };
-  const handleNumberInputChange = () => {};
 
+  // return true on duplicate telephones
+  const checkDulicateTelephones = (telephone) => {
+    const contactsTelephones = contacts.map((contact) => contact.telephone);
+    return contactsTelephones.includes(telephone);
+  };
+
+  // returns true on exact duplicates
   const isDuplicate = (contact) => {
-    return checkDuplicateNames(contact.name); // decoupled to allow scaling
+    return (
+      checkDuplicateNames(contact.name) &&
+      checkDulicateTelephones(contact.telephone)
+    ); // decoupled to allow scaling
   };
 
   const handleAddingContact = (event) => {
-    if (newName !== "") {
+    if (newContact.name !== "" && newContact.telephone !== "") {
       event.preventDefault();
 
-      const contact = {
-        id: contacts.length + 1,
-        name: newName,
-      };
-
-      if (!isDuplicate(contact)) {
-        console.log(newName);
-        setContacts(contacts.concat(contact));
-        setNewName("");
-      } else {
-        alert(`${newName} is already added to phonebook`);
+      if (isDuplicate(newContact))
+        alert(`${newContact.name} is already added to phonebook`);
+      else if (checkDulicateTelephones(newContact.telephone))
+        alert(`${newContact.telephone} `);
+      else {
+        console.log(newContact.name);
+        setContacts(contacts.concat(newContact));
+        setNewContact({
+          ...newContact,
+          name: "",
+          telephone: "",
+        });
       }
     }
   };
 
   const handleNameInputChange = (event) => {
-    setNewName(event.target.value);
+    setNewContact({ ...newContact, name: event.target.value });
+  };
+
+  const handleTelephoneInputChange = (event) => {
+    setNewContact({ ...newContact, telephone: event.target.value });
   };
 
   return (
@@ -46,13 +67,17 @@ const App = () => {
       <form>
         <div>
           name:{" "}
-          <input value={newName} onChange={handleNameInputChange} required />
+          <input
+            value={newContact.name}
+            onChange={handleNameInputChange}
+            required
+          />
         </div>
         <div>
-          number:{" "}
+          telephone:{" "}
           <input
-            value={newNumber}
-            onChange={handleNumberInputChange}
+            value={newContact.telephone}
+            onChange={handleTelephoneInputChange}
             required
           />
         </div>
