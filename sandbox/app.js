0fd39520685bf78323ba4930147135475cfb671d
Ibrahim Aldarra <ibrahim.aldarra010101@gmail.com>
2023-03-10 10:20:06 +0100

    the blogs skeleton is done

--- sandbox/app.js
+++ sandbox/app.js
@@ -4,21 +4,31 @@ const express = require('express')
 const app = express();
 
 
+const blogs = [
+    {"title": "My first blog!", "body": "Eu pariatur et dolor laborum laborum commodo nostrud tempor occaecat esse enim. Qui aliqua dolore eu ea laboris cupidatat mollit ea cupidatat laborum ullamco laboris sit. Anim adipisicing tempor magna excepteur ad esse sint id minim."},
+    {"title": "My second blog!", "body": "Eu pariatur et dolor laborum laborum commodo nostrud tempor occaecat esse enim. Qui aliqua dolore eu ea laboris cupidatat mollit ea cupidatat laborum ullamco laboris sit. Anim adipisicing tempor magna excepteur ad esse sint id minim."},
+    {"title": "My third blog!", "body": "Eu pariatur et dolor laborum laborum commodo nostrud tempor occaecat esse enim. Qui aliqua dolore eu ea laboris cupidatat mollit ea cupidatat laborum ullamco laboris sit. Anim adipisicing tempor magna excepteur ad esse sint id minim."},
+    {"title": "My fourth blog!", "body": "Eu pariatur et dolor laborum laborum commodo nostrud tempor occaecat esse enim. Qui aliqua dolore eu ea laboris cupidatat mollit ea cupidatat laborum ullamco laboris sit. Anim adipisicing tempor magna excepteur ad esse sint id minim."},
+];
+
+app.set('view engine','ejs');
+
 app.listen(3000);
 
 
 app.get("/", (req, resp) => {
-    resp.sendFile("./views/index.html", { root: __dirname });
+    resp.render('index', { title: 'Home', blogs });
 });
 
 app.get("/about", (req, resp) => {
-    resp.sendFile("./views/about.html", { root: __dirname });
+    resp.render('about', { title: 'About'});
 });
 
-app.get("/about-me", (req, resp) => {
-    resp.redirect("/about");
-})
+app.get("/blogs/create", (req, resp) => {
+    resp.render('create', { title: 'Create blog' });
+});
 
-app.get("/*", (req, resp) => {
-    resp.status(404).sendFile("./views/404.html", { root: __dirname });
+
+app.use((req, resp) => {
+    resp.status(404).render('404', { title: 'Page not found'});
 });
